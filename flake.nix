fc9d2df88ae3d0436774cb1d8381d806ba289bce
Stylix <anhduongfa@gmail.com>
2026-05-09 07:11:39 +0700

    first init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Devshell for Zig project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zig,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [
+          zig.overlays.default
+        ];
+
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.zigpkgs.default
+
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.eza
+            pkgs.fd
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+
+            echo "Zig dev shell ready"
+            zig version
+          '';
+        };
+      }
+    );
+}
